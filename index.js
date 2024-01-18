c576110a04efbbe878356c377dcef4d0cb0551f3
Enrique <enrique_dj.tp@outlook.com>
2024-01-17 21:27:53 -0600

    añade funciones de operacion

--- index.js
+++ index.js
@@ -37,52 +37,125 @@ let operacion = "";
 const result = document.querySelector(".result input");
 
 // selector de botón de número
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
 const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
 console.log(seven);
 
-// añadir monitores de eventos / Event Listeners a los elementos
+// selector de boton de Operaciones
+
+const plusButton = document.getElementById("plus");             // suma
+const minusButton = document.getElementById("minus");           // resta
+const multiplyButton = document.getElementById("multiply");     // Multiplicacion
+const divisionButton = document.getElementById("division");     // Division
+const percentageButton = document.getElementById("percentage"); // Porcentake
+const cButton = document.getElementById("C");
+
+
+// añadir monitores de eventos / Event Listeners a los elementos de Numeros
+ 
+one.addEventListener("click", function () {
+  concatenar(one);
+});
+two.addEventListener("click", function () {
+  concatenar(two);
+});
+three.addEventListener("click", function () {
+  concatenar(three);
+});
+four.addEventListener("click", function () {
+  concatenar(four);
+});
+five.addEventListener("click", function () {
+  concatenar(five);
+});
+six.addEventListener("click", function() {
+  concatenar(six);
+});
 seven.addEventListener("click", function () {
   concatenar(seven);
 });
-
-const eight = document.getElementById("eight");
 // cuando pasamos parámetros a la función callback
 // necesitamos envolver nuestra función en una función anónima
 eight.addEventListener("click", function () {
   concatenar(eight);
 });
-
-function concatenar(boton) {
-  // si el primer valor es 0
-  // asignamos a valor1
-  if (!valor1) {
-    result.value += boton.innerText;
-    valor1 = Number(result.value);
-  }
-  // si es diferente de 0
-  // asignamos a valor2
-  else {
-    if (result.value != 0) {
-      result.value += boton.innerText;
-    } else {
-      result.value = boton.innerText;
-    }
-    valor2 = Number(result.value);
-  }
-}
-
-const nine = document.getElementById("nine");
 // cuando pasamos parámetros a la función callback
 // necesitamos envolver nuestra función en una función anónima
 nine.addEventListener("click", function () {
   concatenar(nine);
 });
+zero.addEventListener("click", function() {
+  concatenar(zero);
+});
 
-// suma
-const plusButton = document.getElementById("plus");
+function concatenar(boton) {
+  // si el primer valor es 0
+  // asignamos a valor1
+  // if (!valor1) {
+  //   result.value += boton.innerText;
+  //   valor1 = Number(result.value);
+  // }
+  // // si es diferente de 0
+  // // asignamos a valor2
+  // else {
+  //   if (result.value != 0) {
+  //     result.value += boton.innerText;
+  //   } else {
+  //     result.value = boton.innerText;
+  //   }
+  //   valor2 = Number(result.value);
+  // }
+
+  if (operacion == "") {
+    result.value += boton.innerText;
+    valor1 = Number(result.value);
+  } else {
+    result.value = result.value
+      ? result.value + boton.innerText
+      : boton.innerText;
+    valor2 = Number(result.value);
+  }
+
+}
+
+// Event Listeners a los elementos de Operaciones
+// SUMA
 plusButton.addEventListener("click", function () {
   operacion = "SUMA";
   result.value = "0";
+});
+// RESTA
+minusButton.addEventListener("click", function () {
+  operacion = "RESTA";
+  result.value = "0";
+} );
+// Multiplicacion
+multiplyButton.addEventListener("click", function(){
+  operacion = "MULTI";
+  result.value = "0";
+});
+// Division
+divisionButton.addEventListener("click", function() {
+  operacion = "DIVISION";
+  result.value = 0;
+});
+// Porcentaje
+percentageButton.addEventListener("click", function (){
+  result.value = valor1 / 100;
+  valor1 = Number(result.value);
+});
+cButton.addEventListener("click", function (){
+  valor1 = 0;
+  result.value = 0;
+  
 });
 
 // calcular resultado con el botón de igual
@@ -91,7 +164,20 @@ equalsButton.addEventListener("click", function () {
   switch (operacion) {
     case "SUMA":
       result.value = valor1 + valor2;
-      valor1 = Number(result.value);
-      valor2 = 0;
+       break;
+    case "RESTA":
+      result.value = valor1 - valor2;
+      break;
+    case "MULTI":
+      result.value = valor1 * valor2;
+      break;
+    case "DIVISION":
+      result.value = valor1 / valor2;
+      break;
+    case "PORCENTAJE":
+      break;
   }
+  valor1 = Number(result.value);
+  valor2 = 0;
+  operacion = "";
 });
